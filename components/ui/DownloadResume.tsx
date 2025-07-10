f21da3bcbd306da45c0111f3af4069e2ff6b0273
Nitthin <132218737+Ni2thin@users.noreply.github.com>
2025-07-10 11:43:25 +0530

    Updated link

--- components/ui/DownloadResume.tsx
+++ components/ui/DownloadResume.tsx
@@ -3,23 +3,9 @@ import React from "react";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 
 const DownloadResume: React.FC = () => {
-  // const [isDownloading, setIsDownloading] = useState(false);
-
-  // const handleDownload = () => {
-  //   setIsDownloading(true);
-  //   // Create a link element to trigger the download
-  //   const link = document.createElement("a");
-  //   link.href = "/Tanishka_Deep.pdf"; // Updated path to the PDF file
-  //   link.download = "Tanishka_Deep.pdf";
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  //   setIsDownloading(false);
-  // };
-
   return (
     <Link
-      href="https://drive.google.com/file/d/1YWeCf9phwX-hv1-Bjn6rb7-JtHPP9VHG/view?usp=share_link"
+      href="https://drive.google.com/file/d/1DmWBFVK6Svm1UCXP_K47iIZZ68pL_JMb/view?usp=sharing"
       className="flex items-center justify-center px-3 py-2 bg-white/20 backdrop-blur-md text-white rounded-md border border-white/10 shadow-lg transition-transform duration-300 hover:bg-white/30 hover:scale-105 hover:cursor-pointer font-medium"
       target="_blank"
     >
